fecf44623c1c573ad51faa6426efeeec48d16d71
Bernard366 <bernardsitorus00@gmail.com>
2024-09-11 20:10:02 +0700

    Update Baru

--- js/script.js
+++ js/script.js
@@ -1,44 +1,49 @@
 function Jumluas() {
 
-      var panjang = document.getElementById("panjang").value;
-      var tinggi = document.getElementById("tinggi").value;
+      let panjang = document.getElementById("panjang").value;
+      let tinggi = document.getElementById("tinggi").value;
       if(panjang <= 0 || tinggi <= 0){
-      alert("Angka tidak boleh kosong atau kurang dari 1!")
+      alert("Angka tidak boleh kosong atau kurang dari 1!");
+      console.log("Masukkan angka terlebih dahulu!");
       }else{
-      var luas = 0.5 * panjang * tinggi;  
+      let luas = 0.5 * panjang * tinggi;  
       document.getElementById("jawab").innerText =  "Jawab: \n" +
                                                     "Luas Segitiga      = 1/2 x " + panjang + " x " + tinggi +"\n"+
                                                     "Hasilnya Adalah    = " + luas;
+      console.log("Hasilnya adalah "+ luas);      
                                                 }
 }
 
   
 function Jumkel() {
-
-      var sisi1 = document.getElementById("sisi1").value;
-      var sisi2 = document.getElementById("sisi2").value;
-      var sisi3 = document.getElementById("sisi3").value;
+      let sisi1 = document.getElementById("sisi1").value;
+      let sisi2 = document.getElementById("sisi2").value;
+      let sisi3 = document.getElementById("sisi3").value;
   
       if (sisi1 === "" || sisi2 === "" || sisi3 === "" || parseInt(sisi1) <= 0 || parseInt(sisi2) <= 0 || parseInt(sisi3) <= 0) {
-            alert("Angka tidak boleh kosong atau kurang dari 1!");
-        } else {
-            sisi1 = parseInt(sisi1);
-            sisi2 = parseInt(sisi2);
-            sisi3 = parseInt(sisi3);
-
-      var keliling = sisi1 + sisi2 + sisi3;
-      document.getElementById("jawab2").innerText =  "Jawab: \n" +
-                                                    "Keliling Segitiga      = " + sisi1 + " + " + sisi2 + " + " + sisi3 +"\n"+
-                                                    "Hasilnya Adalah    = " + keliling;
+          alert("Angka tidak boleh kosong atau kurang dari 1!");
+          console.log("Masukkan angka terlebih dahulu!");
+      } else {
+          sisi1 = parseInt(sisi1);
+          sisi2 = parseInt(sisi2);
+          sisi3 = parseInt(sisi3);
+  
+          let keliling = sisi1 + sisi2 + sisi3;
+          document.getElementById("jawab2").innerText =  "Jawab: \n" +
+                                                        "Keliling Segitiga      = " + sisi1 + " + " + sisi2 + " + " + sisi3 +"\n"+
+                                                        "Hasilnya Adalah    = " + keliling;
+          console.log("Hasilnya adalah "+ keliling);
       }
-}
+  }
 
 function hapus(){
 
             document.getElementById("panjang").value = "";
             document.getElementById("tinggi").value = "";
             document.getElementById("jawab").innerText = "";
-}
+            console.log("Sudah dihapus")
+
+      }
 
 
 function hapus2(){
@@ -47,4 +52,6 @@ function hapus2(){
       document.getElementById("sisi2").value= "";
       document.getElementById("sisi3").value= "";
       document.getElementById("jawab2").innerText =  "";
+      console.log("Sudah dihapus")
+
 }
